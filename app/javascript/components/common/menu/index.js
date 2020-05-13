1a070818e3a0eeb84740fe83c381f743fa3d5458
mendesmcmg <mariagoncalves@cjr.org.br>
2020-05-12 22:20:55 -0300

    Styles Homescreen

--- app/javascript/components/common/menu/index.js
+++ app/javascript/components/common/menu/index.js
@@ -1,28 +1,50 @@
 import React, { Fragment } from "react";
-import { Navbar, Container, Columns, Button, Dropdown } from "react-bulma-components";
-import logoImage from '../../../assets/images/logo.png'
-import styled from 'styled-components';
-import { useLocation } from 'react-router-dom';
-import { faUserCircle } from 'react-icons/fa';
+import {
+  Navbar,
+  Container,
+  Columns,
+  Button,
+  Dropdown,
+} from "react-bulma-components";
+import logoImage from "../../../assets/images/logo.png";
+import styled from "styled-components";
+import { useLocation } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
 
 const ColumnsFullWidth = styled(Columns)`
-    width:100%;
-    padding-top: 20px;
-    padding-top: 10px;
-    padding-top: 10px;
-`
-	
+  width: 100%;
+  padding-top: 20px;
+  padding-top: 10px;
+  padding-top: 10px;
+`;
+
 const Menu = () => {
   let actionButton;
- 
-  if (useLocation().pathname == '/') {
-    actionButton = <a href='/users/sign_in' className="is-pulled-right is-right">
-        <Button outlined={true} color="white">ENTRAR</Button>
-      </a>
-  } else {
-   act
-  }
 
+  if (useLocation().pathname == "/") {
+    actionButton = (
+      <a href="/users/sign_in" className="is-pulled-right is-right">
+        <Button outlined={true} color="white">
+          ENTRAR
+        </Button>
+      </a>
+    );
+  } else {
+    actionButton = (
+        <Dropdown
+          className="is-pulled-right is-right"
+          color="dark"
+          label={<FaUserCircle size="2em" />}
+        >
+          <Dropdown.Item value="other">
+            <a href="/users/edit">Edit User</a>
+          </Dropdown.Item>
+          <Dropdown.Item value="other">
+            <a href="/users/sign_out">LogOut</a>
+          </Dropdown.Item>
+        </Dropdown>
+      );
+  }
 
   return (
     <Fragment>
@@ -30,11 +52,9 @@ const Menu = () => {
         <Container>
           <ColumnsFullWidth className="is-mobile">
             <Columns.Column desktop={{ size: 2 }} mobile={{ size: 5 }}>
-                <img src={logoImage} className='image'/>
+              <img src={logoImage} className="image" />
             </Columns.Column>
-            <Columns.Column>
-              {actionButton}
-            </Columns.Column>
+            <Columns.Column>{actionButton}</Columns.Column>
           </ColumnsFullWidth>
         </Container>
       </Navbar>
