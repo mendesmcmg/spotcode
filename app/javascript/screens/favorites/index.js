9084d7c3168acdcdf4db79d847b20227ca90148d
mendesmcmg <mariagoncalves@cjr.org.br>
2020-05-14 20:34:47 -0300

    NavbarFooter

--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer/index.js';
 
 const FavoritesScreen = () => {
     return(
         <Fragment>
             <div>Favorites Screen</div>
+            <NavbarFooter/>
         </Fragment>
     )
 }
